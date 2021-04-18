f28986e45d133350a1ea49968d287515a2db65dc
Fyuga <progfyuga@gmail.com>
2021-04-18 12:41:37 +0900

    テスト２

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -86,6 +86,7 @@
 
                 <div class="links">
                     <a href="">変更</a>
+                    
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
