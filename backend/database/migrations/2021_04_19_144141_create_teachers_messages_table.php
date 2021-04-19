1aefb3c16de9bbe00f928054fbcb8fd1917836c4
shintarou <thirdman_shin@yahoo.co.jp>
2021-04-19 23:52:44 +0900

    migrationファイルを作成

--- backend/database/migrations/2021_04_19_144141_create_teachers_messages_table.php
+++ backend/database/migrations/2021_04_19_144141_create_teachers_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTeachersMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('teachers_messages', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('student_name',255);
-            $table->string('address',255);
-            $table->string('tel_no',255);
-            $table->string('parent_name',255);
+            $table->string('subject', 255);
+            $table->string('content', 255);
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('teachers_messages');
     }
 }
