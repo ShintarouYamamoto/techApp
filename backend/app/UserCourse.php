63e15831b148aa1157c7b013c7b21b3ab1db4efb
DESKTOP-6VVC26L\IO-782 <s_yamamoto@netvalue-io.com>
2021-04-28 16:11:11 +0900

    ユーザ情報編集機能、users_coursesテーブルの複合キー設定、route修正、エラーページのカスタマイズページ生成

--- backend/app/UserCourse.php
+++ backend/app/UserCourse.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserCourse extends Model
 {
-    //
+    protected $table = 'users_courses';
+
+    protected $primaryKey = ['user_id', 'course_id'];
+
+    protected $fillable = [
+        'user_id',
+        'course_id',
+    ];
+    
 }
