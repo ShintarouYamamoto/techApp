69635d38adeeade5d5e6c85af669bf095353ebc4
Fyuga <progfyuga@gmail.com>
2021-05-11 00:20:07 +0900

    生徒一覧表示

--- backend/app/Http/Controllers/Admin/UsersController.php
+++ backend/app/Http/Controllers/Admin/UsersController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     public function index(){
-        return view('admin.users');
+
+        $users = User::sortable()
+            ->orderBy('id', 'desc')
+            ->paginate(5);
+
+        return view('admin.users',['users' => $users]);
+
     }
 }
--- backend/app/User.php
+++ backend/app/User.php
@@ -5,10 +5,13 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Kyslik\ColumnSortable\Sortable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use Sortable;
+    public $sortable = ['id'];
 
     /**
      * The attributes that are mass assignable.
