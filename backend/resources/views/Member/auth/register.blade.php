58e0ded1cd182620b86ee2d4338d473cab905a40
shintarou <thirdman_shin@yahoo.co.jp>
2021-05-17 20:34:39 +0900

    名前input横並び

--- backend/resources/views/Member/auth/register.blade.php
+++ backend/resources/views/Member/auth/register.blade.php
@@ -37,25 +37,27 @@
                 <form method="POST" action="{{ route('member.register') }}">
                     @csrf
 
-                    {{--生徒姓--}}
-                    <div class="form-group">
-                        <label for="student_lastname" class="">生徒姓</label>
-                        <input id="student_lastname" class="form-control @error('student_lastname') is-invalid @enderror" name="student_lastname" value="{{ old('student_lastname') }}" required　autocomplete="name">
+                    {{--生徒姓--}}{{--生徒名--}}
+                    <div class="form-group row">
+                        <div class="col-6">
+                            <label for="student_lastname" class="">生徒姓</label>
+                            <input id="student_lastname" class="form-control @error('student_lastname') is-invalid @enderror" name="student_lastname" value="{{ old('student_lastname') }}" required　autocomplete="name">
+                        </div>
+                        <div class="col-6">
+                            <label for="student_firstname" class="">生徒名</label>
+                            <input id="student_firstname" class="form-control @error('student_firstname') is-invalid @enderror" name="student_firstname" value="{{ old('student_firstname') }}" required　autocomplete="name">
+                        </div>
                     </div>
-                    {{--生徒姓フリガナ--}}
-                    <div class="form-group">
-                        <label for="student_lastname_kana" class="">生徒姓フリガナ</label>
-                        <input id="student_lastname_kana" class="form-control @error('student_lastname_kana') is-invalid @enderror" name="student_lastname_kana" value="{{ old('student_lastname_kana') }}" required>
-                    </div>
-                    {{--生徒名--}}
-                    <div class="form-group">
-                        <label for="student_firstname" class="">生徒名</label>
-                        <input id="student_firstname" class="form-control @error('student_firstname') is-invalid @enderror" name="student_firstname" value="{{ old('student_firstname') }}" required　autocomplete="name">
-                    </div>
-                    {{--生徒名フリガナ--}}
-                    <div class="form-group">
-                        <label for="student_firstname_kana" class="">生徒名フリガナ</label>
-                        <input id="student_firstname_kana" class="form-control @error('student_firstname_kana') is-invalid @enderror" name="student_firstname_kana" value="{{ old('student_firstname_kana') }}" required>
+                    {{--生徒姓フリガナ--}}{{--生徒名フリガナ--}}
+                    <div class="form-group row">
+                        <div class="col-6">
+                            <label for="student_lastname_kana" class="">生徒姓フリガナ</label>
+                            <input id="student_lastname_kana" class="form-control @error('student_lastname_kana') is-invalid @enderror" name="student_lastname_kana" value="{{ old('student_lastname_kana') }}" required>
+                        </div>
+                        <div class="col-6">
+                            <label for="student_firstname_kana" class="">生徒名フリガナ</label>
+                            <input id="student_firstname_kana" class="form-control @error('student_firstname_kana') is-invalid @enderror" name="student_firstname_kana" value="{{ old('student_firstname_kana') }}" required>
+                        </div>
                     </div>
                     {{--メールアドレス--}}
                     <div class="form-group">
@@ -72,46 +74,46 @@
                         <label for="tel_no" class="">電話番号</label>
                         <input id="tel_no" type="tel" class="form-control @error('tel_no') is-invalid @enderror" name="tel_no" value="{{ old('tel_no') }}" required　autocomplete="tel">
                     </div>
-                    {{--保護者姓--}}
-                    <div class="form-group">
-                        <label for="parent_lastname" class="">保護者性</label>
-                        <input id="parent_lastname" type="" class="form-control @error('parent_lastname') is-invalid @enderror" name="parent_lastname" value="{{ old('parent_lastname') }}" required>
+                    {{--保護者姓--}}{{--保護者名--}}
+                    <div class="form-group row">
+                        <div class="col-6">
+                            <label for="parent_lastname" class="">保護者性</label>
+                            <input id="parent_lastname" type="" class="form-control @error('parent_lastname') is-invalid @enderror" name="parent_lastname" value="{{ old('parent_lastname') }}" required>
+                        </div>
+                        <div class="col-6">
+                            <label for="parent_firstname" class="">保護者名</label>
+                            <input id="parent_firstname" type="" class="form-control @error('parent_firstname') is-invalid @enderror" name="parent_firstname" value="{{ old('parent_firstname') }}" required>
+                        </div>
                     </div>
-                    {{--保護者姓フリガナ--}}
-                    <div class="form-group">
-                        <label for="parent_lastname_kana" class="">保護者性フリガナ</label>
-                        <input id="parent_lastname_kana" type="" class="form-control @error('parent_lastname_kana') is-invalid @enderror" name="parent_lastname_kana" value="{{ old('parent_lastname_kana') }}" required>
+                    {{--保護者姓フリガナ--}}{{--保護者名フリガナ--}}
+                    <div class="form-group row">
+                        <div class="col-6">
+                            <label for="parent_lastname_kana" class="">保護者性フリガナ</label>
+                            <input id="parent_lastname_kana" type="" class="form-control @error('parent_lastname_kana') is-invalid @enderror" name="parent_lastname_kana" value="{{ old('parent_lastname_kana') }}" required>
+                        </div>
+                        <div class="col-6">
+                            <label for="parent_firstname_kana" class="">保護者名フリガナ</label>
+                            <input id="parent_firstname_kana" type="" class="form-control @error('parent_firstname_kana') is-invalid @enderror" name="parent_firstname_kana" value="{{ old('parent_firstname_kana') }}" required>
+                        </div>
                     </div>
-                    {{--保護者名--}}
                     <div class="form-group">
-                        <label for="parent_firstname" class="">保護者名</label>
-                        <input id="parent_firstname" type="" class="form-control @error('parent_firstname') is-invalid @enderror" name="parent_firstname" value="{{ old('parent_firstname') }}" required>
-                    </div>
-                    {{--保護者名フリガナ--}}
-                    <div class="form-group">
-                        <label for="parent_firstname_kana" class="">保護者名フリガナ</label>
-                        <input id="parent_firstname_kana" type="" class="form-control @error('parent_firstname_kana') is-invalid @enderror" name="parent_firstname_kana" value="{{ old('parent_firstname_kana') }}" required>
-                    </div>
-                    {{--ログインパスワード--}}
-                    <div class="form-group">
-                        <label for="password" class="">ログインパスワード</label>
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
-                    </div>
-                    {{--ログインパスワード確認--}}
-                    <div class="form-group">
-                        <label for="password_confirmation" class="">ログインパスワード確認</label>
-                        <input id="password_confirmation" type="password" class="form-control @error('password') is-invalid @enderror" name="password_confirmation" required>
-                    </div>
+                        {{--ログインパスワード--}}
+                        <div class="form-group">
+                            <label for="password" class="">ログインパスワード</label>
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
+                        </div>
+                        {{--ログインパスワード確認--}}
+                        <div class="form-group">
+                            <label for="password_confirmation" class="">ログインパスワード確認</label>
+                            <input id="password_confirmation" type="password" class="form-control @error('password') is-invalid @enderror" name="password_confirmation" required>
+                        </div>
 
-
-                    <div class="form-group text-center">
-                        <button type="submit" class="register-button">登録</button>
-                    </div>
-
+                        <div class="form-group text-center">
+                            <button type="submit" class="register-button">登録</button>
+                        </div>
                 </form>
             </div>
         </div>
-
         @include('Parts.footer')
 </body>
 
