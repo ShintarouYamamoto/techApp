1f81f94c7b7f13dab27ac874226177fd4c990bbe
shintarou <thirdman_shin@yahoo.co.jp>
2021-05-20 00:30:53 +0900

    ログインページ、レジスターページレスポンシブ

--- backend/resources/views/Member/auth/login.blade.php
+++ backend/resources/views/Member/auth/login.blade.php
@@ -5,6 +5,7 @@
     <title>ログインページ</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
