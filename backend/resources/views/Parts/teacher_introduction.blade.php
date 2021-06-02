4cb089f9c5719dcb014bf3d8daca954a309beeea
Fyuga <progfyuga@gmail.com>
2021-06-03 00:02:47 +0900

    講師紹介、順番以外のレスポンシブ

--- backend/resources/views/Parts/teacher_introduction.blade.php
+++ backend/resources/views/Parts/teacher_introduction.blade.php
@@ -4,16 +4,16 @@
 <div class="contents">
     <div class="teacher-introduce-title py-5">
         <p>
-            現役の学生エンジニアが教えるから分かりやすい!!
+            現役の学生エンジニアが<br>教えるから分かりやすい!!
         </p>
     </div>
     <div class="teacher-introduce-message">
         <div class="teacher-wrapper">
-            <div class="row mx-0 px-5">
-                <div class="teacher-image col-sm-4 col-12">
+            <div class="row teacher-content">
+                <div class="teacher-image col-lg-4 col-12">
                     <img src="{{ asset('/images/teacher_image01.png') }}" alt="">
                 </div>
-                <div class="teacher-message col-sm-8 col-12">
+                <div class="teacher-message col-lg-8 col-12 ">
                     <p>
                         どうも！真太郎です！どうも！真太郎です！どうも！真太郎です！どうも！真太郎です！<br>
                         どうも！真太郎です！どうも！真太郎です！どうも！真太郎です！どうも！真太郎です！<br>
@@ -22,16 +22,16 @@
                 </div>
             </div>
         </div>
-        <div class="teacher-wrapper row mx-0">
-            <div class="row mx-0 px-5">
-                <div class="teacher-message col-sm-8 col-12">
+        <div class="teacher-wrapper">
+            <div class="row teacher-content">
+                <div class="teacher-message col-lg-8 col-12">
                     <p>
                         どうも！真太郎です！どうも！真太郎です！どうも！真太郎です！どうも！真太郎です！<br>
                         どうも！真太郎です！どうも！真太郎です！どうも！真太郎です！どうも！真太郎です！<br>
                         どうも！真太郎です！どうも！真太郎です！どうも！真太郎です！どうも！真太郎です！<br>
                     </p>
                 </div>
-                <div class="teacher-image col-sm-4 col-12">
+                <div class="teacher-image col-lg-4 col-12">
                     <img src="{{ asset('/images/teacher_image01.png') }}" alt="">
                 </div>
             </div>
